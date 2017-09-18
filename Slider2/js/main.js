2cb40fd6ef9334ffe5581abce9f2df2bf18c8134
Daniel Cataño Restrepo <dasniel199410@gmail.com>
2017-09-18 18:37:14 -0500

    btn

--- Slider2/js/main.js
+++ Slider2/js/main.js
@@ -1,21 +1,34 @@
-var index = 0
 var images = document.getElementById("bg-container").children
+var index = 0
+var timeout
 
-function start(){
-    var interval = setInterval(swap, 5000)
-}
+swap()
 
 function swap(){
     if(index == images.length - 1){
         images[index].className += " hidden"
+        var btn = document.getElementById("btn"+index).firstElementChild        
+        btn.className = btn.className.replace(" inner-button-active", "")      
         index = 0
         images[index].className = images[index].className.replace(" hidden", "")
+        var nextbtn = document.getElementById("btn"+index).firstElementChild
+        nextbtn.className += " inner-button-active"       
     }else{
         images[index].className += " hidden"
         images[index + 1].className = images[index + 1].className.replace(" hidden", "")
+        var btn = document.getElementById("btn"+index).firstElementChild
+        var nextbtn = document.getElementById("btn"+(index + 1)).firstElementChild
+        btn.className = btn.className.replace(" inner-button-active", "")
+        nextbtn.className += " inner-button-active"
         index++;
     }
-    console.log(index)
+    timeout = setTimeout(swap, 5000);
 }
 
-start();
+function setIndex(val, e){
+    e.preventDefault()
+    index = val
+    clearTimeout(timeout)
+    console.log(index)
+    swap()
+}
